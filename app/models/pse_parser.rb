4928f4a9d45e5461f0e7870ec5eb5c7674c23e46
Bryan Bibat <bry@bryanbibat.net>
2015-03-16 17:01:32 +0800

    Add end of day technical calculation

--- app/models/pse_parser.rb
+++ app/models/pse_parser.rb
@@ -215,4 +215,65 @@ class PseParser
       end
     end
   end
+
+  def self.calculate_day_technicals
+    Security.all.each do |s|
+      day_logs = s.stock_day_logs.order(:created_at).all
+      if day_logs.length >= 10
+        day_logs[-1].sma_10 = (day_logs[-10..-1].sum(&:closing_price) / 10).round(3)
+      end
+
+      if day_logs.length == 12
+        day_logs[11].ema_12 = (day_logs[0..11].sum(&:closing_price) / 12).round(3)
+      end
+      if day_logs.length > 12
+        day_logs[-1].ema_12 = (day_logs[-1].closing_price * (2.0/13.0) + day_logs[-2].ema_12 * (1.0 - (2.0/13.0))).round(3)
+      end
+
+      if day_logs.length == 15
+        day_logs[14].ema_14_up = (day_logs[1..14].sum(&:upward_change) / 14).round(3)
+        day_logs[14].ema_14_down = (day_logs[1..14].sum(&:downward_change) / 14).round(3)
+      end
+
+      if day_logs.length > 15
+        day_logs[-1].ema_14_up = (day_logs[-1].upward_change * (2.0/15.0) + day_logs[-2].ema_14_up * (1.0 - (2.0/15.0))).round(3)
+        day_logs[-1].ema_14_down = (day_logs[-1].downward_change * (2.0/15.0) + day_logs[-2].ema_14_down * (1.0 - (2.0/15.0))).round(3)
+
+        if day_logs[-1].ema_14_down < 0.001
+          if day_logs[-1].ema_14_up < 0.001 
+            day_logs[-1].rsi = 50.0
+          else
+            day_logs[-1].rsi = 100.0
+          end
+        else
+          day_logs[-1].rsi = (100.0 - (100.0/(1.0+(day_logs[-1].ema_14_up / day_logs[-1].ema_14_down)))).round(3)
+        end
+      end
+
+      if day_logs.length == 26
+        day_logs[25].ema_26 = (day_logs[0..25].sum(&:closing_price) / 26).round(3)
+      end
+
+      if day_logs.length > 26
+        day_logs[-1].ema_26 = (day_logs[-1].closing_price * (2.0/27.0) + day_logs[-2].ema_26 * (1.0 - (2.0/27.0))).round(3)
+      end
+
+      if day_logs.length == 30
+        day_logs[29].ema_30 = (day_logs[0..29].sum(&:closing_price) / 30).round(3)
+      end
+      if day_logs.length > 30
+        day_logs[-1].ema_30 = (day_logs[-1].closing_price * (2.0/31.0) + day_logs[-2].ema_30 * (1.0 - (2.0/31.0))).round(3)
+      end
+
+      if day_logs.length == 34
+        day_logs[34].macd_signal = (day_logs[25..34].sum(&:macd) / 9).round(3)
+        day_logs[34].macd_divergence = day_logs[34].macd - day_logs[34].macd_signal 
+      end
+      if day_logs.length > 34
+        day_logs[-1].macd_signal = (day_logs[-1].macd * (2.0/10.0) + day_logs[-2].macd_signal * (1.0 - (2.0/10.0))).round(3)
+        day_logs[-1].macd_divergence = day_logs[-1].macd - day_logs[-1].macd_signal 
+      end
+      day_logs[-1].save
+    end
+  end
 end
